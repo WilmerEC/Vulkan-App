286535384615502c4b93d141c3f25fed9a085b6f
Wilmer <wilmerecp1009@outlook.com>
2024-05-31 20:11:44 -0700

    Edited src/CMakeLists.txt
    Added ability to compile with debug info or with '-O2' optimization flag for release mode.

--- src/VulkanApp.cpp
+++ src/VulkanApp.cpp
@@ -8,13 +8,6 @@
 
 int main(int argc, char** argv)
 {
-    // Test
-
-    uint8_t temp1 = 5;
-    uint8_t temp2 = 4;
-    uint8_t result = temp1 & temp2;
-    // std::cout <<  "\ntemp1: 0x" << std::hex << temp1 << "\ntemp2: 0x" << std::hex << temp2 << "\nresult: "  << (temp1 & temp2);
-    std::cout << "\nLuis es marico"; // Easter Egg
 
     Tutorial_Triangle app;
 
